aa09a444fb2e36e18bcb3d883883984d6bbac1d8
MJ-thunder <manasvijain2710@gmail.com>
2025-10-25 00:08:26 +0530

    Resolve merge conflict and integrate difficulty estimator feature

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-const { generateSudoku } = require("./generator");
+const { generateSudoku, getDifficultyLevels } = require("./generator");
 const { solveSudoku } = require("./solver");
 const { isBoardValid } = require("./validator");
+const { SudokuGameManager } = require("./gameManager");
+const { InteractiveSudokuGame } = require("./interactiveGame");
 const { estimateDifficulty } = require("./difficultyEstimator");
 
 // ✅ Export everything together
 module.exports = {
   generateSudoku,
+  getDifficultyLevels,
   solveSudoku,
   isBoardValid,
+  SudokuGameManager,
+  InteractiveSudokuGame,
   estimateDifficulty,
 };
