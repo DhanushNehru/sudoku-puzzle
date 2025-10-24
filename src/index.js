e09df2fea1baa9193cfdae0e0afe2d311b99b6ae
MJ-thunder <manasvijain2710@gmail.com>
2025-10-25 00:03:45 +0530

    Add Sudoku difficulty estimator and update documentation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-const { generateSudoku } = require('./generator');
-const { solveSudoku } = require('./solver');
-const { isBoardValid } = require('./validator');
+const { generateSudoku } = require("./generator");
+const { solveSudoku } = require("./solver");
+const { isBoardValid } = require("./validator");
+const { estimateDifficulty } = require("./difficultyEstimator");
 
+// ✅ Export everything together
 module.exports = {
-    generateSudoku,
-    solveSudoku,
-    isBoardValid
+  generateSudoku,
+  solveSudoku,
+  isBoardValid,
+  estimateDifficulty,
 };
